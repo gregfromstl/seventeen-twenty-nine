24393e4223e05d94447b8ab0dcd3008bc5601e95
gregrolwes <gregory.rolwes@slu.edu>
2021-08-12 18:29:57 -0500

    Added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const { guessProductionMode } = require("@ngneat/tailwind");
+
+module.exports = {
+  prefix: "",
+  purge: {
+    enabled: guessProductionMode(),
+    content: ["./src/**/*.{html,ts}"],
+  },
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "ghost-white": "#f7f7ff",
+        "spring-green": "5efc8d",
+        blue: "#279Af1",
+        "coral-black": "#60656f",
+        "orange-soda": "#f06449",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
