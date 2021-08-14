249c48e251ccf3f167ef126f55918264af445287
gregrolwes <gregory.rolwes@slu.edu>
2021-08-14 16:28:48 -0500

    Added card formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { NavButtonComponent } from './components/nav-button/nav-button.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [AppComponent, NavButtonComponent, NavbarComponent],
+  declarations: [AppComponent, NavButtonComponent, NavbarComponent, CardListComponent, CardComponent],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
