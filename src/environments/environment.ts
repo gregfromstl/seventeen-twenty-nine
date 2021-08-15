df45da6a02e72bc0bc6de95bf04f7c962724913f
gregrolwes <gregory.rolwes@slu.edu>
2021-08-15 11:37:32 -0500

    Removed legacy modules

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,6 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-require('dotenv').config();
-
 export const environment = {
   production: false,
   firebaseConfig: {
