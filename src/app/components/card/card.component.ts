07e007bd6b0aa8b59b78b7bae4238a982cd15e1b
Greg <rolwesg@gmail.com>
2021-08-29 12:30:10 -0500

    Display actual tags

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CardComponent implements OnInit {
   @Input() img: string;
   @Input() title: string;
-  @Input() tags: string[] = [];
+  @Input() tags: string[] | undefined;
 
   constructor() {}
 
